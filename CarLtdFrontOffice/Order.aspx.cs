39d2fa0d809894003b5bd076be9c1cf5eac6c875
p2603585 <p2603585@my365.dmu.ac.uk>
2022-02-25 18:44:55 +0000

    finished

--- CarLtdFrontOffice/Order.aspx.cs
+++ CarLtdFrontOffice/Order.aspx.cs
@@ -22,8 +22,49 @@ public partial class Order : System.Web.UI.Page
     {
         //create an instance of clsOrder
         clsOrder AnOrder = new clsOrder();
+
+        //OrderNo
         //capture the Order no
         AnOrder.OrderNo = Int32.Parse(txtOrderNo.Text);
+        //store the order in the session object
+        Session["AnOrder"] = AnOrder;
+        //redirect to the viewer page
+        Response.Redirect("OrderViewer.aspx");
+
+        //OrderName
+        //capture the Order name
+        AnOrder.OrderName = txtOrderName.Text;
+        //store the order in the session object
+        Session["AnOrder"] = AnOrder;
+        //redirect to the viewer page
+        Response.Redirect("OrderViewer.aspx");
+
+        //OrderStatus
+        //capture the Order status
+        AnOrder.OrderStatus = txtOrderStatus.Text;
+        //store the order in the session object
+        Session["AnOrder"] = AnOrder;
+        //redirect to the viewer page
+        Response.Redirect("OrderViewer.aspx");
+
+        //OrderDate
+        //capture the Order date
+        AnOrder.OrderDate = DateTime.Parse(txtOrderDate.Text);
+        //store the order in the session object
+        Session["AnOrder"] = AnOrder;
+        //redirect to the viewer page
+        Response.Redirect("OrderViewer.aspx");
+
+        //BillingAddress
+        //capture the Billing address
+        AnOrder.OrderStatus = txtBillingAddress.Text;
+        //store the order in the session object
+        Session["AnOrder"] = AnOrder;
+        //redirect to the viewer page
+        Response.Redirect("OrderViewer.aspx");
+
+        //Active
+        AnOrder.Active = Active.Checked;
         //store the order in the session object
         Session["AnOrder"] = AnOrder;
         //redirect to the viewer page
