16026b4cff66f46580f98fd06a4b796a99782baf
p2603585 <p2603585@my365.dmu.ac.uk>
2022-03-06 19:25:09 +0000

    created stored procedure

---
+++ sproc_tblOrder_FilterByOrderNo.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE sproc_tblOrder_FilterByOrderNo
+--parameter to store the order no we are looking for
+	@OrderNo int
+
+AS
+--selct all fields from the table where the order no matches the parameter data
+	SELECT * FROM tblOrder WHERE OrderNo = @OrderNo
+
+RETURN 0
