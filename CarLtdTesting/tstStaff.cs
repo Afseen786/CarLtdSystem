0cdff085c6f5e525dcf89e660c1f0da7d1b860bb
ViolaManzini <p15209829@my365.dmu.ac.uk>
2022-03-07 18:35:53 +0000

    Added testing for staff class

---
+++ CarLtdTesting/tstStaff.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CarLtdTesting
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //An instance of the class to be created
+            clsStaff AStaff = new clsStaff();
+            //Test to see that it exists
+            Assert.IsNotNull(AStaff);
+        }
+    }
+
+    class clsStaff
+    {
+    }
+}
