5cc9478a2eb6112884d55073561fb25e8ea44047
p2603585 <p2603585@my365.dmu.ac.uk>
2022-03-11 20:53:03 +0000

    modified the Find function

--- CarLtdClasses/clsOrder.cs
+++ CarLtdClasses/clsOrder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 
 namespace CarLtdClasses
 {
@@ -11,9 +12,33 @@ namespace CarLtdClasses
         public string OrderStatus { get; set; }
         public string BillingAddress { get; set; }
 
-        public bool Find(int testData)
+        public bool Find(int OrderNo)
         {
-            return true;
+            //create an instance of  the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the order no to search for
+            DB.AddParameter("@OrderNo", OrderNo);
+            //execute the stored procedure
+            DB.Execute("sproc_tblOrder_FilterByOrderNo");
+            //if one record is found (there should be either one or zero!)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                mOrderNo = Convert.ToInt32(DB.DataTable.Rows[0]["OrderNo"]);
+                mOrderName = Convert.ToString(DB.DataTable.Rows[0]["OrderName"]);
+                mOrderStatus = Convert.ToString(DB.DataTable.Rows[0]["OrderStatus"]);
+                mOrderDate = Convert.ToDateTime(DB.DataTable.Rows[0]["OrderDate"]);
+                mBillingAddress = Convert.ToString(DB.DataTable.Rows[0]["BillingAddress"]);
+                mActive = Convert.ToBoolean(DB.DataTable.Rows[0]["Active"]);
+                //return that everything worked OK
+                return true;
+            }
+            //if no record was found
+            else
+            {
+                //return false indicating a problem
+                return false;
+            }
         }
     }
 }
