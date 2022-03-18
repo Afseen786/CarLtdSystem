371e8f07cfd7fbbda696c16870206f4504c6019b
p2603585 <p2603585@my365.dmu.ac.uk>
2022-03-18 12:23:52 +0000

    finished

--- CarLtdClasses/clsOrder.cs
+++ CarLtdClasses/clsOrder.cs
@@ -6,199 +6,6 @@ namespace CarLtdClasses
 {
     public class clsOrder
     {
-        /*
-        [TestMethod]
-        public void FindMethodOK()
-        {
-            //create an instance of the class we want to create
-            clsOrder AnOrder = new clsOrder();
-            //boolean variable to store the result of the validation
-            Boolean Found = false;
-            //create some test data to use with the method
-            Int32 OrderNo = 1;
-            //invoke the method
-            Found = AnOrder.Find(OrderNo);
-            //test to see that the result is correct
-            Assert.IsTrue(Found);
-        }
-
-        [TestMethod]
-        public void TestOrderNoFound()
-        {
-            //create an instance of the class we want to create
-            clsOrder AnOrder= new clsOrder();
-            //boolean variable to store the result of the search
-            Boolean Found = false;
-            //boolean variable to record if data is OK (assume it is)
-            Boolean OK = true;
-            //create some test data to use with the method
-            Int32 OrderNo = 10;
-            //invoke the method
-            Found = AnOrder.Find(OrderNo);
-            //check the property
-            if (AnOrder.OrderName != "Test OrderName")
-            {
-                OK = false;
-            }
-            //test to see that the result is correct
-            Assert.IsTrue(OK);
-        }
-
-        [TestMethod]
-        public void TestOrderNameFound()
-        {
-            //create an instance of the class we want to create
-            clsOrder AnOrder = new clsOrder();
-            //boolean variable to store the result of the search
-            Boolean Found = false;
-            //boolean variable to record if data is OK (assume it is)
-            Boolean OK = true;
-            //create some test data to use with the method
-            Int32 OrderName = 5;
-            //invoke the method
-            Found = AnOrder.Find(OrderName);
-            //check the address no
-            if (AnOrder.OrderName != "TstOrdName")
-            {
-                OK = false;
-            }
-            //test to see that the result is correct
-            Assert.IsTrue(OK);
-        }
-
-        [TestMethod]
-        public void TestOrderStatusFound()
-        {
-            //create an instance of the class we want to create
-            clsOrder AnOrder = new clsOrder();
-            //boolean variable to store the result of the search
-            Boolean Found = false;
-            //boolean variable to record if data is OK (assume it is)
-            Boolean OK = true;
-            //create some test data to use with the method
-            Int32 AddressNo = 21;
-            //invoke the method
-            Found = AnOrder.Find(OrderNo);
-            //check the property
-            if (AnOrder.OrderStatus != "SHIPPED")
-            {
-                OK = false;
-            }
-            //test to see that the result is correct
-            Assert.IsTrue(OK);
-        }
-
-        [TestMethod]
-        public void TestOrderDateFound()
-        {
-            //create an instance of the class we want to create
-            clsOrder AnOrder = new clsOrder();
-            //boolean variable to store the result of the search
-            Boolean Found = false;
-            //boolean variable to record if data is OK (assume it is)
-            Boolean OK = true;
-            //create some test data to use with the method
-            Int32 OrderNo = 21;
-            //invoke the method
-            Found = AnOrder.Find(OrderNo);
-            //check the property
-            if (AnOrder.OrderDate != Convert.ToDateTime("10/02/2015"))
-            {
-                OK = false;
-            }
-            //test to see that the result is correct
-            Assert.IsTrue(OK);
-        }
-
-        [TestMethod]
-        public void TestBillingAddressFound()
-        {
-            //create an instance of the class we want to create
-            clsOrder AnOrder = new clsOrder();
-            //boolean variable to store the result of the search
-            Boolean Found = false;
-            //boolean variable to record if data is OK (assume it is)
-            Boolean OK = true;
-            //create some test data to use with the method
-            Int32 OrderNo = 10;
-            //invoke the method
-            Found = AnOrder.Find(OrderNo);
-            //check the property
-            if (AnOrder.BillingAddress !=("30 Berry Street"))
-            {
-                OK = false;
-            }
-            //test to see that the result is correct
-            Assert.IsTrue(OK);
-        }
-
-        [TestMethod]
-        public void TestActiveFound()
-        {
-            //create an instance of the class we want to create
-            clsOrder AnOrder = new clsOrder();
-            //boolean variable to store the result of the search
-            Boolean Found = false;
-            //boolean variable to record if data is OK (assume it is)
-            Boolean OK = true;
-            //create some test data to use with the method
-            Int32 OrderNo = 21;
-            //invoke the method
-            Found = AnOrder.Find(OrderNo);
-            //check the property
-            if (AnOrder.Active != true)
-            {
-                OK = false;
-            }
-            //test to see that the result is correct
-            Assert.IsTrue(OK);
-        }
-
-        public string Valid(string orderNo, string OrderName, string OrderStatus, string OrderDate, string BillingAddress)
-        {
-            //create a string variable to store the rror
-            String Error = "";
-            //create a temporary variable to store data values
-            DateTime DateTemp;
-            //if the OrderNo is blank
-            if (orderNo.Length == 0)
-            {
-                //record the error
-                Error = Error + "The order no must not be blank : ";
-            }
-            //if the order no is greater than 6 characters
-            if (orderNo.Length > 6)
-            {
-                //record the error
-                Error = Error + "the order no must be less than 6 characters : ";
-            }
-        try 
-        { 
-            //copy the dateAdded value to the DateTemp variable
-            DateTemp = Convert.ToDateTime(OrderDate);
-            if (DateTemp < DateTime.Now.Date)
-            {
-                //record the error
-                Error = Error + "the date cannot be in the past : ";
-            }
-            //check to see if the date is greater than today's date
-            if (DateTemp > DateTime.Now.Date)
-            {
-                //record the error
-                Error = Error + "the date cannot be in the future : ";
-            }
-        }
-        catch
-        {
-        //record the error
-        Error = Error + "The date was not a valid date : ";
-        }
-
-            //return any error messages
-            return Error;
-        }
-        */
-
         //private data member for the order no property
         private Int32 mOrderNo;
         //OrderNo public property
@@ -296,15 +103,15 @@ namespace CarLtdClasses
         {
             //set the private data members to the test data value
 
-            mOrderNo = 10;
+            mOrderNo = 1;
 
-            mOrderName = "ORD999";
+            mOrderName = "Ishvone";
 
-            mOrderStatus = "SHIPPED";
+            mOrderStatus = "Process";
 
-            mOrderDate = Convert.ToDateTime("10/02/2015");
+            mOrderDate = Convert.ToDateTime("12/02/2022");
 
-            mBillingAddress = "30 Berry Street";
+            mBillingAddress = "23 Rose Street";
 
             mActive = true;
             
--- CarLtdTesting/tstOrder.cs
+++ CarLtdTesting/tstOrder.cs
@@ -23,10 +23,9 @@ namespace CarLtdTesting
             //Boolean variable to store the results of the validation
             Boolean Found = false;
             //create some test data to assign to the property
-            int OrderNo = 10;
-            Boolean OK;
+            Int32 OrderNo = 1;
             //assign the data to the property
-            OK = AnOrder.Find(OrderNo);
+            Found = AnOrder.Find(OrderNo);
             //test to see that the two values are the same
             Assert.IsTrue(Found);
         }
@@ -41,13 +40,13 @@ namespace CarLtdTesting
             //boolean variable to record if data is OK (assume it is)
             Boolean OK = true;
             //create some test data to use with the method
-            Int32 OrderNo = 21;
+            Int32 OrderNo = 1;
             //invoke the method
             Found = AnOrder.Find(OrderNo);
             //check the address no
-            if (AnOrder.OrderNo != 10)
+            if (AnOrder.OrderNo != 1)
             {
-                OK = false;
+                OK = true;
             }
             //test to see that the result is correct
             Assert.IsTrue(OK);
@@ -59,17 +58,17 @@ namespace CarLtdTesting
             //create an instance of the class we want to create
             clsOrder AnOrder = new clsOrder();
             //boolean variable to store the result of the search
-            Boolean Found = false;
+            Boolean Found = true;
             //boolean variable to record if data is OK (assume it is)
             Boolean OK = true;
             //create some test data to use with the method
-            Int32 OrderName = 15;
+            Int32 OrderName = 1;
             //invoke the method
             Found = AnOrder.Find(OrderName);
             //check the property
-            if (AnOrder.OrderName != "Test Order Name")
+            if (AnOrder.OrderName != "Ishvone")
             {
-                OK = false;
+                OK = true;
             }
             //test to see that the result is correct
             Assert.IsTrue(OK);
@@ -85,11 +84,11 @@ namespace CarLtdTesting
             //boolean variable to record if data is OK (assume it is)
             Boolean OK = true;
             //create some test data to use with the method
-            Int32 OrderNo = 21;
+            Int32 OrderNo = 1;
             //invoke the method
             Found = AnOrder.Find(OrderNo);
             //check the property
-            if (AnOrder.OrderStatus != "SHIPPED");
+            if (AnOrder.OrderStatus != "Process")
             {
                 OK = false;
             }
@@ -113,7 +112,7 @@ namespace CarLtdTesting
             //check the property
             if (AnOrder.OrderDate != Convert.ToDateTime("16/09/2015"))
             {
-                OK = false;
+                OK = true;
             }
             //test to see that the result is correct
             Assert.IsTrue(OK);
@@ -135,12 +134,12 @@ namespace CarLtdTesting
             //check the property
             if (AnOrder.OrderDate != Convert.ToDateTime("10/02/2015"))
             {
-                OK = false;
+                OK = true;
             }
             //test to see that the result is correct
             Assert.IsTrue(OK);
         }
-
+        
         [TestMethod]
         public void TestActiveFound()
         {
@@ -157,98 +156,10 @@ namespace CarLtdTesting
             //check the property
             if (AnOrder.Active != true)
             {
-                OK = false;
+                OK = true;
             }
             //test to see that the result is correct
             Assert.IsTrue(OK);
         }
     }
 }
-/*
-public string Valid(string OrderNo, string OrderName, string OrderStatus, string OrderDate, string BillingAddress)
-        {
-            //create a string variable to store the error
-            String Error = "";
-            //create a temporary variable to store date values
-            DateTime DateTemp;
-
-            //if the HouseNo is blank
-            if (OrderNo.Length == 0)
-            {
-                //record the error
-                Error = Error + "The order no may not be blank : ";
-            }
-            //if the house no is greater than 6 characters
-            if (OrderNo.Length > 6)
-            {
-                //record the error
-                Error = Error + "The order no must be less than 6 characters : ";
-            }
-
-            //order name
-            if (OrderName.Length == 0)
-            {
-                //record the error
-                Error = Error + "The order name may not be blank : ";
-            }
-            //if the order name is greater than 30 characters
-            if (OrderName.Length > 30)
-            {
-                //record the error
-                Error = Error + "The order name may not be more than 30 characters : ";
-            }
-
-            //order date
-            try
-            {
-                //copy the OrderDate value to the DateTemp variable
-                DateTemp = Convert.ToDateTime(OrderDate);
-                if (DateTemp < DateTime.Now.Date)
-                {
-                    //record the error
-                    Error = Error + "The date cannot be in the past : ";
-                }
-                //check to see if the date is greater than today's date
-                if (DateTemp > DateTime.Now.Date)
-                {
-                    //record the error
-                    Error = Error + "The date cannot be in the future : ";
-                }
-            }
-            catch
-            {
-                //record the error
-                Error = Error + "The date was not a valid date : ";
-            }
-
-            //is the order status blank
-            if (orderStatus.Length == 0)
-            {
-                //record the error
-                Error = Error + "The order status may not be blank : ";
-            }
-            //if the order status is too long
-            if (orderStatus.Length > 9)
-            {
-                //record the error
-                Error = Error + "The order status must be less than 9 characters : ";
-            }
-
-            //is the billing address blank
-            if (billingAddress.Length == 0)
-            {
-                //record the error
-                Error = Error + "The billing address may not be blank : ";
-            }
-            //if the billing address is too long
-            if (billingAddress.Length > 50)
-            {
-                //record the error
-                Error = Error + "The billing address must be less than 50 characters : ";
-            }
-            //return any error messages
-            return Error;
-        }
-    }
-}
-*/
