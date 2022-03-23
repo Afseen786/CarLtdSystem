e12bb3203dbf38bd213958e278ed7eecc106a2e7
P2603822 <P2603822@GRHXX005WIN058W>
2022-03-23 14:21:03 +0000

    completed

---
+++ CarLtdClasses/clsSupplierCollection.cs
@@ -0,0 +1,41 @@
+﻿using CarLtdTesting;
+using System.Collections.Generic;
+
+namespace CarLtdClasses
+{
+    public class clsSupplierCollection
+    {
+        //private data member for the list
+        List<clsSupplier> mSuplierList = new List<clsSupplier>();
+        //public property for the address list
+
+
+        public int Count
+        {
+            get
+            {
+                return mSuplierList.Count;
+            }
+            set
+            {
+
+            }
+
+
+        }
+
+        public List<clsSupplier> SupplierList
+        {
+            get
+            {
+                return mSuplierList;
+            }
+            set
+            {
+                mSuplierList = value;
+            }
+        }
+
+        public clsSupplier ThisSupplier { get; set; }
+    }
+}
